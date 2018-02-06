baa6e4c99f79d60167fb5dd4025162d663636641
philiz <h6zhan@gmail.com>
2018-02-06 22:49:43 +0800

    finished part 6

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,23 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-// import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 // import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { HomeComponent } from './components/home/home.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ProfileComponent } from './components/profile/profile.component';
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { HomeComponent } from "./components/home/home.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { ProfileComponent } from "./components/profile/profile.component";
+
+import { ValidateService } from "./services/validate.service";
+import { FlashMessagesModule } from "angular2-flash-messages";
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'profile', component: ProfileComponent }
+  { path: "", component: HomeComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "login", component: LoginComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "profile", component: ProfileComponent }
 ];
 
 @NgModule({
@@ -32,11 +35,12 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    // FormsModule,
+    FormsModule,
     // HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule.forRoot()
   ],
-  providers: [],
+  providers: [ValidateService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ValidateService } from "../../services/validate.service";
+import { FlashMessagesService } from "angular2-flash-messages";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent implements OnInit {
+  name: String;
+  username: String;
+  email: String;
+  password: String;
 
-  constructor() { }
+  constructor(
+    private validateService: ValidateService,
+    private flashMessage: FlashMessagesService
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onRegisterSubmit() {
+    const user = {
+      name: this.name,
+      username: this.username,
+      email: this.email,
+      password: this.password
+    };
+    // required fields
+    if (!this.validateService.validateRegister(user)) {
+      this.flashMessage.show("请提供所有信息", {
+        cssClass: "alert-danger",
+        timeout: 2000
+      });
+      return false;
+    }
+    // validate email
+    if (!this.validateService.validateEmail(user.email)) {
+      this.flashMessage.show("无效邮箱", {
+        cssClass: "alert-danger",
+        timeout: 2000
+      });
+      return false;
+    }
   }
-
 }
