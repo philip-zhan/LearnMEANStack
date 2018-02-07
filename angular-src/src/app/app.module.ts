e49b9e9f64eaa1ef02d400d35ac9944ee3aa727d
philiz <h6zhan@gmail.com>
2018-02-07 11:43:46 +0800

    finished part 7

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,26 +1,27 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-// import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from "@angular/router";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from "./app.component";
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { LoginComponent } from "./components/login/login.component";
-import { RegisterComponent } from "./components/register/register.component";
-import { HomeComponent } from "./components/home/home.component";
-import { DashboardComponent } from "./components/dashboard/dashboard.component";
-import { ProfileComponent } from "./components/profile/profile.component";
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-import { ValidateService } from "./services/validate.service";
-import { FlashMessagesModule } from "angular2-flash-messages";
+import { ValidateService } from './services/validate.service';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+import { AuthService } from './services/auth.service';
 
 const appRoutes: Routes = [
-  { path: "", component: HomeComponent },
-  { path: "register", component: RegisterComponent },
-  { path: "login", component: LoginComponent },
-  { path: "dashboard", component: DashboardComponent },
-  { path: "profile", component: ProfileComponent }
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
@@ -36,11 +37,11 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    // HttpModule,
+    HttpModule,
     RouterModule.forRoot(appRoutes),
     FlashMessagesModule.forRoot()
   ],
-  providers: [ValidateService],
+  providers: [ValidateService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from "@angular/core";
-import { ValidateService } from "../../services/validate.service";
-import { FlashMessagesService } from "angular2-flash-messages";
+import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: "app-register",
-  templateUrl: "./register.component.html",
-  styleUrls: ["./register.component.css"]
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
   name: String;
@@ -15,7 +17,9 @@ export class RegisterComponent implements OnInit {
 
   constructor(
     private validateService: ValidateService,
-    private flashMessage: FlashMessagesService
+    private flashMessage: FlashMessagesService,
+    private authService: AuthService,
+    private router: Router
   ) {}
 
   ngOnInit() {}
@@ -27,21 +31,40 @@ export class RegisterComponent implements OnInit {
       email: this.email,
       password: this.password
     };
+
     // required fields
     if (!this.validateService.validateRegister(user)) {
-      this.flashMessage.show("请提供所有信息", {
-        cssClass: "alert-danger",
+      this.flashMessage.show('请提供所有信息', {
+        cssClass: 'alert-danger',
         timeout: 2000
       });
       return false;
     }
+
     // validate email
     if (!this.validateService.validateEmail(user.email)) {
-      this.flashMessage.show("无效邮箱", {
-        cssClass: "alert-danger",
+      this.flashMessage.show('无效邮箱', {
+        cssClass: 'alert-danger',
         timeout: 2000
       });
       return false;
     }
+
+    // register user
+    this.authService.registerUser(user).subscribe(data => {
+      if (data.success) {
+        this.flashMessage.show('注册成功', {
+          cssClass: 'alert-success',
+          timeout: 2000
+        });
+        this.router.navigate(['/login']);
+      } else {
+        this.flashMessage.show('注册失败', {
+          cssClass: 'alert-danger',
+          timeout: 2000
+        });
+        this.router.navigate(['/register']);
+      }
+    });
   }
 }
