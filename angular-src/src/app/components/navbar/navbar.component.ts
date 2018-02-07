a1cdbdc43592a6d280f8f62bc41e1049d46fbf8f
philiz <h6zhan@gmail.com>
2018-02-07 14:41:56 +0800

    finished part 8

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private flashMessages: FlashMessagesService
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onLogoutClick() {
+    this.authService.logout();
+    this.flashMessages.show('成功退出登录', {
+      cssClass: 'alert-success',
+      timeout: 2000
+    });
+    this.router.navigate(['/login']);
+    return false;
   }
-
 }
--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -16,4 +16,27 @@ export class AuthService {
       .post('http://localhost:3000/users/register', user, { headers: headers })
       .map(res => res.json());
   }
+
+  authenticateUser(user) {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http
+      .post('http://localhost:3000/users/authenticate', user, {
+        headers: headers
+      })
+      .map(res => res.json());
+  }
+
+  storeUserData(token, user) {
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
+
+  logout() {
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
+  }
 }
